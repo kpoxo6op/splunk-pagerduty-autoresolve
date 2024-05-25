a7c6af2ddf8375e7c3221f242e1079ab4a744769
kpoxo6op <kpoxo6op@gmail.com>
2024-05-25 14:26:37 +1200

    pd terraform

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "splunk/splunk"
       version = "1.4.22"
     }
+    pagerduty = {
+      source  = "pagerduty/pagerduty"
+      version = ">= 2.2.1"
+    }
   }
 }
 
@@ -12,6 +16,43 @@ provider "splunk" {
   username             = "admin"
   password             = "abcd1234"
   insecure_skip_verify = true
+}
+
+provider "pagerduty" {
+  token = "u+7JZYRUsmEcTkKwstYg"
+}
+
+resource "pagerduty_service" "demo_service" {
+  name              = "Demo Service"
+  escalation_policy = "PIOVCT7"
+}
+
+resource "pagerduty_event_orchestration" "resolve" {
+  name        = "Resolve"
+  description = "Resolves alert from Splunk"
+
+}
+
+resource "pagerduty_event_orchestration_service" "resolve" {
+  service = pagerduty_service.demo_service.id
+  set {
+    id = "start"
+    rule {
+      label = "Autotoresolve Splunk Alerts"
+      condition {
+        expression = "event.custom_details.event_action matches 'resolve'"
+      }
+      actions {
+        event_action = "resolve"
+      }
+    }
+  }
+
+  catch_all {
+    actions {
+
+    }
+  }
 }
 
 resource "splunk_saved_searches" "search" {
@@ -41,11 +82,13 @@ resource "splunk_saved_searches" "search" {
     | where event_action!=event_action_lookup
     | table event_action, date_last_run, date_last_change
   EOT
-  actions = "pagerduty"
-  action_pagerduty_integration_url = "https://events.pagerduty.com/integration/92348d67ea9f4b0ed01008c8b440f353/enqueue"
-  cron_schedule = "*/5 * * * *"
-  alert_condition = "search count > 10"
-  description = "Resolvable PagerDuty Alert Demo"
+  # actions = "pagerduty"
+  # action_pagerduty_integration_url = "https://events.pagerduty.com/integration/92348d67ea9f4b0ed01008c8b440f353/enqueue"
+  # cron_schedule = "*/5 * * * *"
+  # alert_condition = "search count > 10"
+  # description = "Boris As Code Errors Alert resolvable by PagerDuty"
+  #{"action": "$result.event_action$"}
+  # action_pagerduty_custom_details = "{\"action\": \"$result.event_action$\"}"
 }
 
 # http://localhost:8000/en-GB/debug/refresh
@@ -53,25 +96,26 @@ resource "splunk_saved_searches" "search" {
 
 resource "splunk_configs_conf" "kvstore-collections-stanza" {
   # /opt/splunk/etc/apps/search/local/collections.conf
-  name      = "collections/state_alert"
+  name = "collections/state_alert"
   variables = {
-    "field._key"          : "string"
-    "field.event_action"  : "string"
-    "field.date_last_change": "number"
+    "field._key" : "string"
+    "field.event_action" : "string"
+    "field.date_last_change" : "number"
     "field.date_last_run" : "number"
   }
 }
 
 resource "splunk_configs_conf" "kvstore-transforms-stanza" {
   # /opt/splunk/etc/apps/search/local/transforms.conf
-  name      = "transforms/state_alert"
+  name = "transforms/state_alert"
   variables = {
-    "collection"    : "state_alert"
+    "collection" : "state_alert"
     "external_type" : "kvstore"
-    "fields_list"   : "_key,event_action,date_last_change,date_last_run"
+    "fields_list" : "_key,event_action,date_last_change,date_last_run"
   }
 }
 
+# pagerduty
 /*
 resource pagerduty service
 
